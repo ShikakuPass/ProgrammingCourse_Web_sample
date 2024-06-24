ffa68bbae4dcc5fb7cddb09652a589e7d2991a26
日下 <dev-shikaku+sample_upload@gakken.co.jp>
2024-06-24 11:10:01 +0900

    first commit

---
+++ HTML基礎_サンプルファイル/第13章/13/receive02.php
@@ -0,0 +1,15 @@
+<?php
+print "性別：";
+print $_REQUEST["SEIBETU"];
+print "<br>";
+
+print "好きな科目：";
+if (isset($_REQUEST["KAMOKU"])) {
+	foreach ($_REQUEST["KAMOKU"] as $data) {
+		print $data;
+	}
+} else {
+	print "なし";
+}
+print "<br>";
+?>
